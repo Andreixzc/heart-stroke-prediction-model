6352d0b4f8bd02c403f0ad6f5d45cd4966ed804d
Andreixzc <andrei.massaini@hotmail.com>
2023-05-22 12:38:09 -0300

    finalizando kpfold

--- Old/Kfold.py
+++ Old/Kfold.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_val_predict, KFold, cross_val_score
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, f1_score, precision_recall_fscore_support, precision_score, recall_score
 from sklearn.tree import DecisionTreeClassifier
 from yellowbrick.classifier import ConfusionMatrix
 import matplotlib.pyplot as plt
@@ -215,16 +215,113 @@ def k_fold_cross_validation_com_grid(k, model, dataset):
     return scores
 
 
+def k_fold_cross_validation_com_grid2(k, model, dataset):
+    X_train = dataset[0]  # Características de treino
+    X_test = dataset[1]  # Características de teste
+    y_train = dataset[2]  # Rótulos de treino
+    y_test = dataset[3]  # Rótulos de teste
+
+    skf = StratifiedKFold(n_splits=k, shuffle=True)  # Divisão em k folds estratificados
+
+    scores = []  # Lista para armazenar as métricas de cada fold
+
+    params = {
+        'criterion':  ['gini', 'entropy'],
+        'max_features': [None, 'sqrt', 'log2', 0.2, 0.4, 0.6, 0.8],
+    }
+
+    f1_scores = []
+    precision_scores = []
+    recall_scores = []
+
+    for train_index, val_index in skf.split(X_train, y_train):
+        X_train_fold, X_val_fold = X_train[train_index], X_train[val_index]  # Dados de treino e validação do fold
+        y_train_fold, y_val_fold = y_train[train_index], y_train[val_index]  # Rótulos de treino e validação do fold
+
+        # Undersampling NearMiss nos dados de treino do fold
+        nm = NearMiss(version=2)
+        X_train_resampled, y_train_resampled = nm.fit_resample(X_train_fold, y_train_fold)
+
+        # GridSearch no fold
+        grid_search = GridSearchCV(estimator=model, param_grid=params, scoring='accuracy', cv=3)
+        grid_search.fit(X_train_resampled, y_train_resampled)
+
+        # Melhores hiperparâmetros encontrados
+        best_params = grid_search.best_params_
+
+        # Treinamento do modelo com os melhores hiperparâmetros
+        model.set_params(**best_params)
+        model.fit(X_train_resampled, y_train_resampled)
+
+        # Avaliação do modelo no conjunto de validação do fold
+        y_pred = model.predict(X_val_fold)
+        score = model.score(X_val_fold, y_val_fold)
+        scores.append(score)
+
+        # Métricas de classificação do fold
+        f1 = f1_score(y_val_fold, y_pred, average=None)
+        precision = precision_score(y_val_fold, y_pred, average=None)
+        recall = recall_score(y_val_fold, y_pred, average=None)
+        f1_scores.append(f1)
+        precision_scores.append(precision)
+        recall_scores.append(recall)
+
+        # Relatório de classificação do fold
+        print(f"Fold {len(scores)}:")
+        print(classification_report(y_val_fold, y_pred))
+        print("------------------------------")
+
+    # Avaliação final no conjunto de teste
+    y_pred_test = model.predict(X_test)
+    final_score = model.score(X_test, y_test)
+    scores.append(final_score)
+
+    # Métricas de classificação do conjunto de teste
+    f1_test = f1_score(y_test, y_pred_test, average=None)
+    precision_test = precision_score(y_test, y_pred_test, average=None)
+    recall_test = recall_score(y_test, y_pred_test, average=None)
+    f1_scores.append(f1_test)
+    precision_scores.append(precision_test)
+    recall_scores.append(recall_test)
+
+    # Cálculo da média do F1-score, precisão e recall para cada classe superior e inferior
+    mean_f1_scores = [sum(scores) / len(scores) for scores in zip(*f1_scores)]
+    mean_precision_scores = [sum(scores) / len(scores) for scores in zip(*precision_scores)]
+    mean_recall_scores = [sum(scores) / len(scores) for scores in zip(*recall_scores)]
+
+    # Exibição das médias
+    print("Média do F1-score para cada classe:")
+    print(mean_f1_scores)
+    print("------------------------------")
+    print("Média da precisão para cada classe:")
+    print(mean_precision_scores)
+    print("------------------------------")
+    print("Média do recall para cada classe:")
+    print(mean_recall_scores)
+    print("------------------------------")
+
+    return scores, f1_scores, precision_scores, recall_scores
+
+
+
+
+
+
+
+
+
+
 rf = RandomForestClassifier()
 dt = DecisionTreeClassifier()
 
 
 print("Decision Tree")
-# print(k_fold_cross_validation_com_grid(5,rf,dataset))
-# print(k_fold_cross_validation_com_grid(5,dt,dataset))
-# print("Randomn forest")
-# k_fold(5,random_forest,dataset)'
-#passar o modelo destreinado
+# print(k_fold_cross_validation_com_grid2(5,dt,dataset))
+scores, f1_scores, precision_scores, recall_scores = k_fold_cross_validation_com_grid(5, dt, dataset)
+print(scores)
+print(f1_scores)
+# print(interval_confidence(k_fold_cross_validation_com_grid(5,dt,dataset)))
+
 
 
 
